c5d89462a4c6dbaa6be4c83d52a322ad8fe2cb0e
krishna1857 <40385296+krishna1857@users.noreply.github.com>
2019-04-12 15:26:10 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,4 +1,7 @@
 import sys
 print(" ------------- HELOOO ------------------")
 print(" ------------- HELOOO ------------------")
-print(" ------------- HELOOO ------------------")
+
+file1 = open("MyFile.txt","a")
+file1.write(" ------------- HELOOO ------------------")
+file1.close() 
