4476c8f3a9b52b8347d5324f1cd550bd7c2ceb8a
krishna1857 <40385296+krishna1857@users.noreply.github.com>
2019-04-12 15:27:13 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -2,6 +2,6 @@ import sys
 print(" ------------- HELOOO ------------------")
 print(" ------------- HELOOO ------------------")
 
-file1 = open("MyFile.txt","a")
+file1 = open("C:\Program Files (x86)\Jenkins\workspace\testing\MyFile.txt","a")
 file1.write(" ------------- HELOOO ------------------")
 file1.close() 
