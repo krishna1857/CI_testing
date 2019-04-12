ce521958dcccaffd69b561fbe96d024c9af18579
krishna1857 <40385296+krishna1857@users.noreply.github.com>
2019-04-12 14:24:31 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,3 +1,5 @@
 import sys
 print(" ------------- HELOOO ------------------")
 sys.stdout.write("Finishing Program")
+print(" ------------- HELOOO ------------------")
+print(" ------------- HELOOO ------------------")
